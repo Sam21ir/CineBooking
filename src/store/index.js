65451bc46859733e2086b36fe3c33dbdae9fe851
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2026-01-27 21:39:12 +0100

    feat: create seatsSlice Redux with seat selection logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import moviesReducer from './slices/moviesSlice'
 import sessionsReducer from './slices/sessionsSlice'
+import seatsReducer from './slices/seatsSlice'
+
 
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
     sessions: sessionsReducer,
+    seats: seatsReducer,
   },
 })
