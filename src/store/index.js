da58a1e7012a72d3c619d5945e371a4704f372aa
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2026-01-27 21:03:27 +0100

    feat: create sessionsSlice Redux with fetchSessions actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import moviesReducer from './slices/moviesSlice'
+import sessionsReducer from './slices/sessionsSlice'
 
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    sessions: sessionsReducer,
   },
 })
