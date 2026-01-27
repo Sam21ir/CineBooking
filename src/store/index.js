714052e245de41a7709698a871f7c646b3f3cf48
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2026-01-27 22:09:57 +0100

    feat: create bookingService and bookingsSlice Redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import moviesReducer from './slices/moviesSlice'
 import sessionsReducer from './slices/sessionsSlice'
 import seatsReducer from './slices/seatsSlice'
+import bookingsReducer from './slices/bookingsSlice'
+
 
 
 export const store = configureStore({
@@ -9,5 +11,6 @@ export const store = configureStore({
     movies: moviesReducer,
     sessions: sessionsReducer,
     seats: seatsReducer,
+    bookings: bookingsReducer,
   },
 })
